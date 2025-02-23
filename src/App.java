8c11cd3003864feb4740de4fb16f8a5f0baf5980
Cristofer <cristofdzfdz@gmail.com>
2025-02-23 01:22:08 +0100

    Modifico clase App para que el metodo devuelva una excepción del error

--- src/App.java
+++ src/App.java
@@ -2,8 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(App.dividir(6, 0));
     }
-    static int dividir(int a, int b) {
-        if (b == 0) return -1; // Devuelve un código de error
+    static int dividir(int a, int b) throws ArithmeticException {
+        if (b == 0) throw new ArithmeticException("División por cero");
         return a / b;
     }
 }
